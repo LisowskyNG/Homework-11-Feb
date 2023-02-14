82e52b339bbfc60aac05f2d6573bf7e960f18e88
Valery Maltsev <lisowsky@yandex.ru>
2023-02-14 16:44:07 +0300

    Task 1 - 3

--- src/Main.java
+++ src/Main.java
@@ -13,18 +13,93 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         // Пишем код для задачи 1
+        int [] first = new int [] {1, 2, 3};
+        double [] second = {1.57, 7.654, 9.986};
+        int [] third = new int [5];
+
 
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
+        int [] first = new int [] {1, 2, 3};
+        double [] second = {1.57, 7.654, 9.986};
+        int [] third = new int [5];
+        for (int i = 0; i < first.length; i++) {
+            System.out.print(first[i]);
+            if (i < first.length-1) {
+                System.out.print(", ");
+            }
+                else {
+                    System.out.println();
+                }
+        }
+
+        for (int i = 0; i < second.length; i++) {
+            System.out.print(second[i]);
+            if (i < second.length-1) {
+                System.out.print(", ");
+            }
+            else {
+                System.out.println();
+            }
+        }
+
+
+        for (int i = 0; i < third.length; i++) {
+            System.out.print(third[i]);
+            if (i < third.length-1) {
+                System.out.print(", ");
+            }
+            else {
+                System.out.println();
+            }
+        }
+
+
 
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         // Пишем код для задачи 3
+
+        int [] first = new int [] {1, 2, 3};
+        double [] second = {1.57, 7.654, 9.986};
+        int [] third = new int [5];
+
+        for (int i = first.length-1; i >= 0; i--) {
+            System.out.print(first[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            } else {
+                System.out.println();
+            }
+
+        }
+
+        for (int i = second.length-1; i >= 0; i--) {
+            System.out.print(second[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            } else {
+                System.out.println();
+            }
+
+        }
+
+        for (int i = third.length-1; i >= 0; i--) {
+            System.out.print(third[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            } else {
+                System.out.println();
+            }
+
+        }
+
+
 
     }
 
